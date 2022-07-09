a8fe3102d7dd05ebc4777133b01063df0efb6f53
Willian Sostenes <sostenes655@gmail.com>
2022-07-09 13:48:08 -0300

    Criando classe role e implementando spring security no projeto

---
+++ src/main/java/curso/api/rest/model/Role.java
@@ -0,0 +1,48 @@
+package curso.api.rest.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity(name = "role")
+@SequenceGenerator(name = "seq_role", sequenceName = "seq_role", allocationSize = 1, initialValue = 1)
+public class Role implements GrantedAuthority {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_role")
+	private Integer id;
+
+	private String nomeRole;
+
+	@Override
+	public String getAuthority() {
+
+		return this.nomeRole;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNomeRole() {
+		return nomeRole;
+	}
+
+	public void setNomeRole(String nomeRole) {
+		this.nomeRole = nomeRole;
+	}
+
+}
