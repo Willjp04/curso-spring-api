e7ea70ff2683e1d70c1a432e4c3ba2abb5632500
Willian Sostenes <sostenes655@gmail.com>
2022-07-09 15:27:06 -0300

    Configurando JWT no projeto

--- src/main/java/curso/api/rest/model/Role.java
+++ src/main/java/curso/api/rest/model/Role.java
@@ -12,9 +12,7 @@ import org.springframework.security.core.GrantedAuthority;
 @SequenceGenerator(name = "seq_role", sequenceName = "seq_role", allocationSize = 1, initialValue = 1)
 public class Role implements GrantedAuthority {
 
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 
 	@Id
---
+++ src/main/java/curso/api/rest/security/WebConfigSecurity.java
@@ -0,0 +1,11 @@
+package curso.api.rest.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
+
+}
