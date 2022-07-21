78025c6b5cc9e5653cfd42a37558a3b94fa528d7
Willian Sostenes <sostenes655@gmail.com>
2022-07-21 12:59:21 -0300

    Criando classes e parametrizando para tratamento de Excecoes

---
+++ src/main/java/curso/api/rest/ObjetoErro.java
@@ -0,0 +1,24 @@
+package curso.api.rest;
+
+public class ObjetoErro {
+
+	private String error;
+	private String codigo;
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+}
